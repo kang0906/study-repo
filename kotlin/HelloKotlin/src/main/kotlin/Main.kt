c19fd09defcfce9220fce117e2a891e522fdc3ef
jinkoo kang <jinkoo906@gmail.com>
2025-02-03 16:20:25 +0900

    hello kotlin

---
+++ kotlin/HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main( ) {
+    println("Hello World!");
+
+    val number = 10
+    var language = "korean"
+    val secondNumber: Int = 20; language = "English"
+
+    println("number = $number")
+    println("language = $language")
+    println("secondNumber = ${secondNumber}")
+}
